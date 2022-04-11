8a6b88c3ba954fdb08a7292cc8c1914042a2eee3
Md. Shykat Emam <36465375+ShykatEmam@users.noreply.github.com>
2022-04-11 14:47:05 +0600

    55

--- music/views.py
+++ music/views.py
@@ -1,19 +1,18 @@
-from cgitb import html
-from  django.http import HttpResponse
 from .models import Album
-from django.shortcuts import render
-from django.http import  Http404
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
     all_albums = Album.objects.all()
     return render(request, 'music/index.html', {'all_albums' : all_albums,})
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    
+    # try:
+    #     album = Album.objects.get(pk = album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist")
+
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album':album})
 
 
