2a9969130d6aedff5880aaa73427dca83ca637a3
Md. Shykat Emam <36465375+ShykatEmam@users.noreply.github.com>
2022-04-11 10:27:24 +0600

    55

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('/', views.index, name='index'),#music
+    
+    path('/<int:album_id>/', views.detail, name='detail'),#music
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,9 @@
 from  django.http import HttpResponse
+from .models import Album
 
 def index(request):
     return HttpResponse("<h1> THis is music app homepage</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for album_id: " + str(album_id) + "</h2>")
+
