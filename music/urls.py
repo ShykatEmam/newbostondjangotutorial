29f5a63b5d54e96e05246a11ec6f205a9c0a159d
Md. Shykat Emam <36465375+ShykatEmam@users.noreply.github.com>
2022-04-11 14:42:01 +0600

    3

--- music/urls.py
+++ music/urls.py
@@ -2,9 +2,13 @@ from unicodedata import name
 from django.urls import path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
-    path('/', views.index, name='index'),#music
+    path('/', views.index, name='index'),
+    #music
     
-    path('/<int:album_id>/', views.detail, name='detail'),#/music/68575/
+    path('/<int:album_id>/', views.detail, name='detail'),
+    #/music/68575/
 
 ]
